ba4868366b005078a3df9fdd8fe7334b7ca8de4d
Tanish Makadia <tanishmakadia@gmail.com>
2023-07-14 00:14:42 -0400

    Serialize dataframe containing symbols to JSON outfile

--- backend/src/nasdaq_listings.py
+++ backend/src/nasdaq_listings.py
@@ -1,16 +1,25 @@
 import requests
 import pandas as pd
 import json
+import os
 
+# request nasdaq listing data
 url = "https://api.nasdaq.com/api/screener/stocks?tableonly=true&limit=25&offset=0&download=true"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0"
 }
 
+# extract symbols from response
 response = requests.get(url, headers=headers, timeout=15)
 response_dict = json.loads(response.content.decode())
-
 rows = response_dict["data"]["rows"]
 df = pd.DataFrame.from_dict(rows)
+df = df["symbol"]
 
-print(df["symbol"])
+# serialize data in JSON format and save on machine
+serialized_json = df.to_json()
+outfile_name = "nasdaq_listings.json"
+outfile_dir = os.path.join(os.getcwd(), "backend", "json")
+
+with open(os.path.join(outfile_dir, outfile_name), "w") as outfile:
+    outfile.write(serialized_json)
