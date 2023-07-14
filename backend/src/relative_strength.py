a2842e6f523105aee484c6c4caba764a84ede24b
Tanish Makadia <tanishmakadia@gmail.com>
2023-07-14 17:44:59 -0400

    Download data for all symbols from yahoo finance

--- backend/src/relative_strength.py
+++ backend/src/relative_strength.py
@@ -8,4 +8,26 @@ json_path = os.path.join(os.getcwd(), "backend", "json", "nasdaq_listings.json")
 df = pd.read_json(json_path, orient="index")
 df.columns = ["Symbol"]
 
-print(df)
+symbol_list = df["Symbol"].values.tolist()
+temp_symbol_list = [
+    "NVDA",
+    "PLTR",
+    "AI",
+    "SLRN",
+    "ATMU",
+    "BAD/TKR",
+    "MDB",
+    "INTC",
+    "IBM",
+    "CRWD",
+    "ELF",
+    "SNOW",
+    "TWLO",
+    "SDGR",
+    "ASML",
+    "ACLS",
+]
+
+tickers = yf.download(temp_symbol_list, period="ytd")
+
+print(tickers["ACLS"])
