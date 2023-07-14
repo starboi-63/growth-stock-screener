d7956734d54eb4abc2608589d160a49439a73ccb
Tanish Makadia <tanishmakadia@gmail.com>
2023-07-14 15:10:23 -0400

    Read serialized JSON symbol data into pandas dataframe

--- backend/src/relative_strength.py
+++ backend/src/relative_strength.py
@@ -1,1 +1,11 @@
 import yfinance as yf
+import os
+import json
+import pandas as pd
+
+# open json data extracted from nasdaq as pandas dataframe
+json_path = os.path.join(os.getcwd(), "backend", "json", "nasdaq_listings.json")
+df = pd.read_json(json_path, orient="index")
+df.columns = ["Symbol"]
+
+print(df)
