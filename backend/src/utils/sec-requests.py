81c6fab3153b07ff0025819f4012873b6fa2b7c1
Tanish Makadia <tanishmakadia@gmail.com>
2023-08-04 02:49:31 -0400

    Define function to convert annual revenue to quarterly revenue

--- backend/src/utils/sec-requests.py
+++ backend/src/utils/sec-requests.py
@@ -1,7 +1,6 @@
 import requests
 from requests.exceptions import JSONDecodeError
 import pandas as pd
-import json
 
 # constants
 header = {"User-Agent": "name@domain.com"}
@@ -36,6 +35,7 @@ def get_concept(symbol: str, concept: str) -> dict:
 
 
 def get_revenues(symbol: str) -> pd.DataFrame:
+    """Fetch quarterly revenue data for a stock symbol from SEC filings."""
     # different companies file revenue with various concepts, and we must check which has the most data
     revenue_concepts = [
         "Revenues",
@@ -59,4 +59,44 @@ def get_revenues(symbol: str) -> pd.DataFrame:
     return revenue_df
 
 
-print(get_revenues("AI"))
+def revenue_growth(frame: str, df: pd.DataFrame) -> float:
+    frame_components = frame.split("Q")
+    year = frame_components[0].replace("CY", "")
+    quarter = None if (len(frame_components) < 2) else frame_components[1]
+
+    if quarter is None:
+        # must convert annual revenue to quarterly revenue by subtracting previous three quarters
+        try:
+            revenue = subtract_prev_quarters(frame, df)
+            print(revenue)
+        except Exception:
+            return None
+
+    return None
+
+
+def subtract_prev_quarters(frame: str, df: pd.DataFrame) -> float:
+    """Convert annual revenue to quarterly revenue by subtracting revenue from the previous three 10-Q SEC filings."""
+    index = df.index.get_loc(df.index[df["frame"] == frame][0])
+    quarter_indices = [index - 3, index - 2, index - 1, index]
+    revenues = []
+
+    if quarter_indices[0] < 0:
+        return None
+
+    for i in quarter_indices:
+        row = df.iloc[i]
+
+        if (i < index) and ("Q" not in row["frame"]):
+            return None
+
+        revenues.append(row["val"])
+
+    revenue = revenues[3] - (revenues[0] + revenues[1] + revenues[2])
+    return revenue
+
+
+df = get_revenues("AAPL")
+print(df)
+print()
+print(revenue_growth("CY2022", df))
