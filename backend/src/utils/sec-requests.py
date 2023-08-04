e6f27211f807738ff6169379d40767e42469c214
Tanish Makadia <tanishmakadia@gmail.com>
2023-08-03 22:52:21 -0400

    Define a function to convert stock symbols to SEC ciks

--- backend/src/utils/sec-requests.py
+++ backend/src/utils/sec-requests.py
@@ -1,15 +1,19 @@
 import requests
+import pandas as pd
+import json
 
 # constants
 header = {"User-Agent": "name@domain.com"}
 
+# get table to convert from stock tickers to cik's
+response = requests.get(
+    "https://www.sec.gov/files/company_tickers.json", headers=header
+)
+conversions_df = pd.DataFrame.from_dict(response.json(), orient="index").set_index(
+    "ticker"
+)
+
 
 def get_cik(symbol: str) -> str:
-    conversions = requests.get(
-        "https://www.sec.gov/files/company_tickers.json", headers=header
-    )
-
-    return conversions
-
-
-print(get_cik("NVDA"))
+    """Convert a stock symbol into a cik used by the SEC for corporate filings."""
+    return conversions_df.loc[symbol]["cik_str"]
