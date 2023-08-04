0313d6bff5cb4d1f742d30af6b8420f24ea66258
Tanish Makadia <tanishmakadia@gmail.com>
2023-08-04 03:36:59 -0400

    Define a function to calculate revenue growth for a given financial timeframe

--- backend/src/utils/sec-requests.py
+++ backend/src/utils/sec-requests.py
@@ -1,6 +1,7 @@
 import requests
 from requests.exceptions import JSONDecodeError
 import pandas as pd
+from calculations import percent_change
 
 # constants
 header = {"User-Agent": "name@domain.com"}
@@ -59,11 +60,11 @@ def fetch_revenues(symbol: str) -> pd.DataFrame:
     return revenue_df
 
 
-def subtract_prev_quarters(frame: str, df: pd.DataFrame) -> float:
+def subtract_prev_quarters(timeframe: str, df: pd.DataFrame) -> float:
     """Convert annual revenue to quarterly revenue by subtracting revenue from the previous three 10-Q SEC filings."""
     # determine the index of the row with revenue for the inputted timeframe
     try:
-        index = df.index.get_loc(df.index[df["frame"] == frame][0])
+        index = df.index.get_loc(df.index[df["frame"] == timeframe][0])
     except IndexError:
         return None
 
@@ -88,32 +89,34 @@ def subtract_prev_quarters(frame: str, df: pd.DataFrame) -> float:
     return revenue
 
 
-def extract_revenue(frame: str, df: pd.DataFrame) -> float:
+def extract_revenue(timeframe: str, df: pd.DataFrame) -> float:
     """Return the revenue for a given timeframe from an SEC revenue DataFrame."""
-    if "Q" in frame:
+    if "Q" in timeframe:
         # search DataFrame for quarterly timeframe
         try:
-            return df[df["frame"] == frame].iloc[0]["val"]
+            return df[df["frame"] == timeframe].iloc[0]["val"]
         except IndexError:
             return None
     else:
         # convert annual revenue into quarterly revenue
-        return subtract_prev_quarters(frame, df)
+        return subtract_prev_quarters(timeframe, df)
 
 
-def previous_frame(frame: str) -> str:
-    """Return an SEC timeframe that is 1 year earlier than the inputted frame."""
-    year = int(frame[2:6]) - 1
-    quarter = frame[6:]
+def previous_frame(timeframe: str) -> str:
+    """Return an SEC timeframe that is 1 year earlier than the inputted timeframe."""
+    year = int(timeframe[2:6]) - 1
+    quarter = timeframe[6:]
     return f"CY{year}{quarter}"
 
 
-def revenue_growth(frame: str, df: pd.DataFrame) -> float:
-    revenue = extract_revenue(frame, df)
-    print(revenue)
-    return None
+def revenue_growth(timeframe: str, df: pd.DataFrame) -> float:
+    """Calculate the revenue growth for the given timeframe compared to the same timeframe one year earlier."""
+    prev_frame = previous_frame(timeframe)
 
+    revenue = extract_revenue(timeframe, df)
+    prev_revenue = extract_revenue(prev_frame, df)
 
-df = fetch_revenues("AAPL")
-print(df)
-print()
+    if (revenue is None) or (prev_revenue is None):
+        return None
+
+    return percent_change(prev_revenue, revenue)
