c27be06d7524005fd9500076500d1c794f7626f3
Tanish Makadia <tanishmakadia@gmail.com>
2023-08-06 21:49:45 -0400

    Remove duplicate import for Selenium Options

--- growth_stock_screener/iterations/utils/concurrency.py
+++ growth_stock_screener/iterations/utils/concurrency.py
@@ -6,7 +6,6 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
 
 
