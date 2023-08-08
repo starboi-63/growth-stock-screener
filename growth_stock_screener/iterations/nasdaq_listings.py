448bb5c26ee8346ed8236db2e1eacc045f779269
Tanish Makadia <tanishmakadia@gmail.com>
2023-08-08 17:32:36 -0400

    Print 'fetching' message before sending get request

--- growth_stock_screener/iterations/nasdaq_listings.py
+++ growth_stock_screener/iterations/nasdaq_listings.py
@@ -16,6 +16,7 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0"
 }
 
+cprint("Fetching stock symbols from NASDAQ . . .", "cyan")
 # extract symbols from response
 try:
     response = requests.get(url, headers=headers, timeout=15)
