eb68373b4a89802d060228a970ce44b4ee75b502
Tanish Makadia <tanishmakadia@gmail.com>
2023-08-09 01:10:28 -0400

    Add custom ASCII art banner

--- growth_stock_screener/run_screen.py
+++ growth_stock_screener/run_screen.py
@@ -3,20 +3,37 @@ from datetime import datetime
 from termcolor import cprint, colored
 
 # print banner
-banner = """
-   ______                   __  __       _____ __             __   
-  / ____/________ _      __/ /_/ /_     / ___// /_____  _____/ /__ 
- / / __/ ___/ __ \ | /| / / __/ __ \    \__ \/ __/ __ \/ ___/ //_/ 
-/ /_/ / /  / /_/ / |/ |/ / /_/ / / /   ___/ / /_/ /_/ / /__/ ,<    
-\____/_/   \____/|__/|__/\__/_/ /_/   /____/\__/\____/\___/_/|_|   
-    _________                                                
-   /   _____/ ___________   ____   ____   ____   ___________ 
-   \_____  \_/ ___\_  __ \_/ __ \_/ __ \ /    \_/ __ \_  __ \\
-   /\___/   \  \___|  | \/\  ___/\  ___/|   |  \  ___/|  | \/
-  /_______  /\___  >__|    \___  >\___  >___|  /\___  >__|   
-          \/     \/            \/     \/     \/     \/       
+banner = """                                                                                                                
+                                                                                                                         
+                                                                                      .    .!YGY                         
+                                                                                    ^BP      ^@B                         
+                              :!!!!^~:  ^7J! ^77^    ^!!77^   .^7?^     !^    :!: ::7@P:::    #G :~7?!.                  
+                            :PG!::~B@! ^!!#@J!?#J  7BY~^^?&B^ ~~!&&:  .5@B    7@P ~~J@P~!^    ##77!7P@5           ~^     
+                           .#&.    P@^    5@^  .  ~@P     7@G    J@7 :Y^J@!   .&?   ~@Y      .#G    .&G      .^75#@J     
+                           !@G     P@^    5@:     J@Y     ~@B    ^@Y^5: .&#. .Y7    ~@Y      .&P    .&G   :7YB@@@@@J     
+                           :&@?. .^G@^    G@:     ^&@~   .P@!    .&@5.   7@5!J^     ~@B:.^^  :@P    :@#::~.  ?@@@B@P     
+                     :~~.   ^NVDA?!B@:    55.      ^5GY??YJ^      Y5.     5P~       .YBPJ!.  :G7     Y#P?^  7@@@? ^J.    
+                  ~AI&@@#?        ^@P        :7??7    ...               .^?J            .^J?...            7@@@Y         
+               :J#@@#@@@@5  YB7^~7PJ.        G@@@@P.            ..:~!?5B&@#P^   ..^~7JPB&@#G##&BY~.       ~@@@B          
+            .7G@&5!^5@@@5   .~7!~^.    ^~^~7GROWTH@#7        YBBSTOCKS5J!^.  PBB##BBBG5J!:  7@@@@@#5~     B@@@!          
+          ^5&#5~. ^G@@@J            ~5B@@@B57^^P@@@@@B7.     ^!&@B~?7        ^?@@G7~         ?@@@&@@@G!  !@@@B           
+       .7B&P!.  .J@@@&7           ^G@@@BJ^.    :&@@5Y&@&Y^    J#@@&G? .^!?YPGGG&@@&7  .^7J5NET@@@B~Y&@@P:5@@@?           
+     :J##J:     AMZN?:     ..:::.^&@@&?         J@@B .~JB@BJ^ !?&@@?JB&@#GY7~^.!@@&?5B&@#PY7~^?#@7  :P@@&&@@@^           
+   .Y&G!       ..:^~!7JY5PPPGPPP5P@@@P      :   ^@@@^    5@@@B^ #@@@@G?^.      :@@@@@P7^       .^     7&@@@@#.           
+  ~#@5^^~!?Y5GGBGPP5YY?~^::..    ~@@@@!   ^5@5   #@@CRWD#PLTRJ: ?JYJ:          .?JY?:                  :^~!??            
+ ~@@@@@@@&ELF?~:. ~J#&&G~      .J&@@@@@#B#@@#J   B@@7:#@@G                                                               
+ :JYJ?7~:.          J@@@#    .?#@@#J^7J55Y?~.   .#@@! ^&@@P                                                              
+                    !@@@B  :Y&@@G7.              J5P^  ~#@@G:                                                            
+                    P@@@7~P@@BJ^                        :P@@&7                                                           
+                   ?@@@&#@#Y~                             7#@@G~                                                         
+                  ^&@@@GJ^                                 .7G@@B?:                                                      
+                   .^!~                                       :7YP57^                                                    
+                                                                              
 """
 cprint(banner, "cyan")
+
+time = datetime.now()
+cprint("" + time.strftime("%m/%d/%Y %H:%M:%S"))
 
 # run screen iterations
 import iterations.nasdaq_listings
@@ -32,9 +49,8 @@ import iterations.relative_strength
 # final_iteration = "institutional_accumulation"
 # df = open_outfile(final_iteration)
 
-# # determine the current time
-# time = datetime.now()
-# time_string = time.strftime("%Y-%m-%d %H-%M-%S")
+
+time_string = time.strftime("%Y-%m-%d %H-%M-%S")
 
 # # create a .csv outfile
 # outfile_name = f"screen_results {time_string}.csv"
