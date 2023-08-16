849bdce564e52f9c72856a624ecc6fcf23757ac6
Tanish Makadia <tanishmakadia@gmail.com>
2023-08-16 19:03:59 -0400

    Change version comparison function to check for '>=' instead of '>'

--- growth_stock_screener/screen/iterations/utils/versions.py
+++ growth_stock_screener/screen/iterations/utils/versions.py
@@ -8,16 +8,24 @@ def python_version() -> str:
 
 def assert_python_updated(min_version: str) -> bool:
     """Assert whether Python is up-to-date with an inputted version number of the form 'x.x.x...'"""
-    return version_greater(python_version(), min_version)
+    return version_geq(python_version(), min_version)
 
 
-def version_greater(vnum_a: str, vnum_b: str) -> bool:
-    "Returns 'True' if version 'a.a.a...' > 'b.b.b...'"
+def version_geq(vnum_a: str, vnum_b: str) -> bool:
+    "Returns 'True' if version 'a.a.a...' >= 'b.b.b...'"
     a_components = vnum_a.split(".")
     b_components = vnum_b.split(".")
-    min_len = min(len(a_components), len(b_components))
+    max_len = max(len(a_components), len(b_components))
 
-    for i in range(min_len):
+    # fill in '0's when version lengths are not the same
+    if len(a_components) < max_len:
+        for i in range(max_len - len(a_components)):
+            a_components.append("0")
+    elif len(b_components) < max_len:
+        for i in range(max_len - len(b_components)):
+            b_components.append("0")
+
+    for i in range(max_len):
         if a_components[i] > b_components[i]:
             return True
         elif a_components[i] < b_components[i]:
